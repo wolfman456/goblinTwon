1bd80dcab7401ddbd3ad20286f546aca6bda2eaf
wolfman456 <pshook456@gmail.com>
2023-03-10 18:44:40 -0600

    start refactoring for typewriter

--- brains.js
+++ brains.js
@@ -1,10 +1,23 @@
 
 const root = document.querySelector("body");
-
-let log = document.getElementById("log");
-let welcome = "hello what is your name"
+const speed = 100;
+let mesaageArray = {
+  "message":["hello what is your name"],
+"nameMessage":[`Would you like to play a game ${playerName}?`]};
+// let mesaageArray = ["hello what is your name"];
 let playerName;
+let i = 0;
+let textPosition = 0;
 
+
+
+typewriter = () => {
+  document.querySelector("#message").innerHTML
+    = mesaageArray.message[i].substring(0, textPosition)
+    + "<span>\u25ae</span>";
+  if (textPosition++ != mesaageArray.message[i].length)
+    setTimeout(typewriter, speed);
+}
 
 function intro() {
   const input = document.createElement("input");
@@ -17,8 +30,9 @@ function intro() {
   root.append(input, startButton);
 
   startButton.addEventListener('click', () => {
+    i++
     playerName = document.getElementById("username").value;
-    log.innerText = `Would you like to play a game ${playerName}?`;
+    
     input.remove();
     startButton.remove();
     const yestBTN = document.createElement("button");
@@ -43,8 +57,8 @@ function optionOne() {
   log.innerText = `GoodBye ${playerName}.`;
 }
 function optionTwo() {
-  log.innerText = 
-  `You wake up in front of a cave ${playerName}.
+  log.innerText =
+    `You wake up in front of a cave ${playerName}.
   There is a terrible smell coming from the cave.
   Do you enter the cave or walk away.`
   const cave = document.createElement('button');
@@ -52,37 +66,51 @@ function optionTwo() {
   const walk = document.createElement('button');
   walk.innerText = "walk away";
   root.append(cave, walk);
- 
-  cave.addEventListener('click', ()=>{
+
+  cave.addEventListener('click', () => {
     cave.remove();
     walk.remove();
     enterCave();
   });
 
-  walk.addEventListener('click', ()=>{
+  walk.addEventListener('click', () => {
     cave.remove();
     walk.remove();
-    leave(); 
+    leave();
   });
 }
 
-function enterCave(){
+function enterCave() {
+  txt = "you enter the cave and smell something funny."
+  
 
 }
 
-function leave(){
+
+function leave() {
   document.body.style.backgroundColor = "red";
-log.innerText = 
-`A goblin walks up behind you
+  log.innerText =
+    `A goblin walks up behind you
 and hit you over the head with a rock.
 you are dead`
+  const playAgain = document.createElement('button');
+  playAgain.innerText = "Play Again"
+  root.append(playAgain);
+  playAgain.addEventListener('click', () => {
+    playAgain.remove;
+    optionTwo();
+  })
+
+}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function mainLoop() {
-  log.innerText = welcome;
+
+  typewriter();
   intro();
-
-
 }
-
 mainLoop();
+
+// window.addEventListener("load", typewriter);
